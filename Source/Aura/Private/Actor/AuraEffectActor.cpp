441c8afac40ecf9faf20d441414725f8a256a0e7
modin <eom3658@gmail.com>
2025-03-03 13:05:04 +0900

    36. Callbacks for Mana Changes

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -37,6 +37,7 @@ void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActo
 
 		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
 		MutableAuraAttributeSet->SetHealth(MutableAuraAttributeSet->GetHealth() + 25.f);
+		MutableAuraAttributeSet->SetMana(MutableAuraAttributeSet->GetMana() - 25.f);
 		Destroy();
 	}
 }
