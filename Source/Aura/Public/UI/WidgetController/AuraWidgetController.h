e22d759fbfd59ecb26bb66d65901ebb6f253eb2f
modin <eom3658@gmail.com>
2025-03-03 12:32:36 +0900

    35. Listening for Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -43,6 +43,7 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetControllerParams(const FWidgetControllerParam& WCParams);
 	virtual void BroadcastInitialValues();
+	virtual void BindCallbacksToDependencies();
 	
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
